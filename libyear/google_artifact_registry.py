6037c5fc0974c5e72647c2b34be89b68f2d993b8
dmaljovec <daniel.maljovec@recursionpharma.com>
2023-10-21 06:37:53 -0600

    add support for all features for internal usage

---
+++ libyear/google_artifact_registry.py
@@ -0,0 +1,100 @@
+import os
+
+from google.cloud import artifactregistry_v1
+from packaging.version import parse
+
+GOOGLE_PROJECT = os.environ.get("GOOGLE_ARTIFACT_REGISTRY_PROJECT", "")
+GOOGLE_LOCATION = os.environ.get("GOOGLE_ARTIFACT_REGISTRY_LOCATION", "us-central1")
+GOOGLE_REPOSITORY = os.environ.get("GOOGLE_ARTIFACT_REGISTRY_PYPI_REPOSITORY", "python")
+
+
+def get_package_details(
+    name,
+    version=None,
+    project=GOOGLE_PROJECT,
+    location=GOOGLE_LOCATION,
+    repository=GOOGLE_REPOSITORY,
+):
+    items = []
+    client = artifactregistry_v1.ArtifactRegistryClient()
+    artifact_name = f"projects/{project}/locations/{location}/repositories/{repository}/packages/{name}"
+    request = artifactregistry_v1.ListVersionsRequest(parent=artifact_name)
+    response = client.list_versions(request=request)
+    for page in response.pages:
+        for version in page.versions:
+            extracted_version = version.name.rsplit("/", 1)[1]
+            if version is None or version == version == extracted_version:
+                items.append(
+                    (
+                        name,
+                        parse(extracted_version),
+                        version.update_time,
+                    )
+                )
+
+    return sorted(items)
+
+
+def get_artifact_registry_timestamp(
+    name,
+    version,
+    project=GOOGLE_PROJECT,
+    location=GOOGLE_LOCATION,
+    repository=GOOGLE_REPOSITORY,
+):
+    client = artifactregistry_v1.ArtifactRegistryClient()
+    artifact_name = f"projects/{project}/locations/{location}/repositories/{repository}/pythonPackages/{name}:{version}"
+    request = artifactregistry_v1.GetPythonPackageRequest(name=artifact_name)
+    response = client.get_python_package(request)
+    return response.update_time
+
+
+def get_version_release_dates(
+    name,
+    version,
+    version_lt,
+    project=GOOGLE_PROJECT,
+    location=GOOGLE_LOCATION,
+    repository=GOOGLE_REPOSITORY,
+):
+    available_versions = [
+        v[1]
+        for v in get_package_details(
+            name=name,
+            project=project,
+            location=locals,
+            repository=repository,
+        )
+    ]
+
+    # retrieve latest_version
+    latest_version = str(available_versions[-1])
+
+    # retrieve version
+    if version_lt:
+        version_lt = parse(version_lt)
+        idx = bisect.bisect_left(available_versions, version_lt)
+        if idx > 0:
+            version = str(available_versions[idx - 1])
+        else:
+            print(f"Unsatisfiable constraint: {name}<{str(version_lt)}", file=sys.stderr)
+            version = None
+
+    if version is None:
+        return None, None, None, None
+
+    version_date = get_artifact_registry_timestamp(
+        name,
+        version,
+        project=project,
+        location=location,
+        repository=repository,
+    )
+    latest_version_date = get_artifact_registry_timestamp(
+        name,
+        latest_version,
+        project=project,
+        location=location,
+        repository=repository,
+    )
+    return version, version_date, latest_version, latest_version_date
